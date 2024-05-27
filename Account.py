5bb1e2c953f627ff0c71a1337f0fa2e13fb4b3c8
nyabangWech <nyabangwech@gmail.com>
2024-05-27 21:52:47 +0300

    added files

---
+++ Account.py
@@ -0,0 +1,13 @@
+# view Account Details  method  to display the Account  Details and Current Balance
+class details:
+    def Account_Details(self,name,Balance):
+        self.name=name
+        self.Balance=Balance
+        print(f"{name}your Account balance{Balance}")
+    name="nyabang" 
+    Balance = 340 
+    #change Account owner:Method to update the Account owners's information
+    
+     
+        
+
