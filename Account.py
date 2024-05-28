de8ed9039edc5f7d27e0d067949f362e2da1598d
nyabangWech <nyabangwech@gmail.com>
2024-05-28 23:09:45 +0300

    added changes

--- Account.py
+++ Account.py
@@ -1,13 +1,34 @@
 # view Account Details  method  to display the Account  Details and Current Balance
 class details:
-    def Account_Details(self,name,Balance):
-        self.name=name
-        self.Balance=Balance
-        print(f"{name}your Account balance{Balance}")
-    name="nyabang" 
-    Balance = 340 
-    #change Account owner:Method to update the Account owners's information
+      def __init__(self, owner,Balance):
+          self.owner=owner
+          self.Balance =Balance
+      def view_details(self):
+          print(f"{self.name} your current balance is{self.Balance}")
+         
+Acc=details("nyabang",1000)
+def  update_owner(self,newowner):
+   self.owner=newowner
+   
+   print("Account upadted")
+   
+   
+def trancastion (self, amount, outlined ):
+ self.Balance+=amount 
+ self.transcation.append((amount,outlined))
+   
+def statement(self):
+    print("Recent transcation")
+    for  transcation  in self.transcation:
+     
     
+    
+         
+        
+    
+        
+    
+        
      
         
 
