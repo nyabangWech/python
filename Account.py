7bb1eecccb99e25189af5c840ae6810e0bdb20e6
nyabangWech <nyabangwech@gmail.com>
2024-05-30 13:01:47 +0300

    added changes

--- Account.py
+++ Account.py
@@ -6,20 +6,53 @@ class details:
       def view_details(self):
           print(f"{self.name} your current balance is{self.Balance}")
          
-Acc=details("nyabang",1000)
-def  update_owner(self,newowner):
-   self.owner=newowner
+      Acc= details("nyabang",1000)
+      def  update_owner(self,newowner):
+       self.owner=newowner
+
+       print("Account upadted")
    
-   print("Account upadted")
    
+      def trancastion (self, amount, outlined ):
+       self.Balance+=amount 
+       self.transcation.append((amount,outlined))
    
-def trancastion (self, amount, outlined ):
- self.Balance+=amount 
- self.transcation.append((amount,outlined))
-   
-def statement(self):
-    print("Recent transcation")
-    for  transcation  in self.transcation:
+      def statement(self):
+       print("Recent transcation")
+       for  self.transcation  in transcation:
+def  setoverdraft_limit(self,limit):
+        setoverdraft_limit = limit
+        if self.Balance<0:
+            self. Balance -= interest_Balance
+def Freeze(self):
+      freeze=True 
+      if(freeze==True):
+           print("Account is Freezed ")
+def unfreeze(self):
+    unfreeze==False
+    if(unfreeze==False):
+        print("Account is unfreezed")
+        
+def transcation(self,withdrawal,deposit):
+     self.withdrawal=withdrawal
+     self.deposit=deposit
+     print( f"you{withdrawal} and received a {deposit} your fee transcation is ksh23")
+def Transfer( self ,reciever,sender,Amount):
+    self.receiver=reciever
+    self.sender=sender
+    self.Amount=Amount
+
+    
+
+
+        
+    
+        
+            
+            
+            
+            
+          
      
     
     
